81cf20a06b2f1d910ae57638a79867e7fef1e7ed
neelgoyani702 <103193259+neelgoyani702@users.noreply.github.com>
2024-07-27 14:59:17 +0530

    change default image url
    Co-authored-by: Divy2103 <110351992+Divy2103@users.noreply.github.com>

--- models/user.model.js
+++ models/user.model.js
@@ -36,7 +36,8 @@ const userSchema = new mongoose.Schema(
     },
     image: {
       type: String,
-      default: "uploads/default.jpg",
+      default:
+        "http://res.cloudinary.com/youtube-clone-backend/image/upload/v1722067886/llys5ceizey72ik1c5kq.png",
     },
   },
   { timestamps: true }
